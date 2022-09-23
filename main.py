1e81a07ce53dda0222b692dacb457d0b385bf0c2
Andreas Ink <andreasink17@gmail.com>
2022-09-23 18:38:37 -0400

    adds data csv

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import streamlit as st
 st.image("./CodeBusters.png", width = 200)
 st.header("AI4Good Hackathon")
 st.subheader("AI4Good Hackathon text")
-baseDF = pd.read_csv("./data/prod_data_2021_train.csv")
+baseDF = pd.read_csv("./data/prod_data_202207_test.csv")
 weatherDF = pd.read_csv("./data/hourly_weather_data_202207_test.csv")
 baseDF = baseDF.rename(columns = {'LDate': "DATE"})
 
-baseDF["DATE"] = pd.to_datetime(baseDF["DATE"])
-weatherDF["DATE"] = pd.to_datetime(weatherDF["DATE"])
+baseDF["DATE"] = pd.to_datetime(baseDF["DATE"]).dt.date
+weatherDF["DATE"] = pd.to_datetime(weatherDF["DATE"]).dt.date
 col1, col2 = st.columns(2)
 col1.write(baseDF)
 col2.write(weatherDF)
@@ -17,4 +17,4 @@ combinedDF = pd.concat([baseDF, weatherDF])
 
 st.write(combinedDF)
 
-# combinedDF.to_csv("./data.csv")
+combinedDF.to_csv("./data.csv")
