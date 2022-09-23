b40b2fdd512144d00a679adbf4b5dfccb54d9492
Andreas Ink <andreasink17@gmail.com>
2022-09-23 18:34:05 -0400

    cleaned data

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ baseDF = pd.read_csv("./data/prod_data_2021_train.csv")
 weatherDF = pd.read_csv("./data/hourly_weather_data_202207_test.csv")
 baseDF = baseDF.rename(columns = {'LDate': "DATE"})
 
-baseDF = pd.to_datetime(baseDF["DATE"])
-weatherDF = pd.to_datetime(weatherDF["DATE"])
+baseDF["DATE"] = pd.to_datetime(baseDF["DATE"])
+weatherDF["DATE"] = pd.to_datetime(weatherDF["DATE"])
 col1, col2 = st.columns(2)
 col1.write(baseDF)
 col2.write(weatherDF)
-combinedDF = pd.merge(baseDF, weatherDF)
+combinedDF = pd.concat([baseDF, weatherDF])
 
 st.write(combinedDF)
+
+# combinedDF.to_csv("./data.csv")
