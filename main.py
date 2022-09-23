dddc6b861007c07f8ea85427ebe1754f698c6275
Andreas Ink <andreasink17@gmail.com>
2022-09-23 19:54:52 -0400

    further data cleaning

--- main.py
+++ main.py
@@ -12,15 +12,24 @@ weatherDF = pd.read_csv("./data/hourly_weather_data_202207_test.csv")
 baseDF = baseDF.rename(columns = {'LDate': "DATE"})
 
 # Format date columns
+baseDF["HOUR"] = pd.to_datetime(baseDF["DATE"]).dt.hour
 baseDF["DATE"] = pd.to_datetime(baseDF["DATE"]).dt.date
+
+weatherDF["HOUR"] = pd.to_datetime(weatherDF["DATE"]).dt.hour
 weatherDF["DATE"] = pd.to_datetime(weatherDF["DATE"]).dt.date
+
+baseDF["AVG"] = baseDF[["Jax Solar", "NW Jax"]].mean(axis = "columns")
+# st.write(baseDF[["Jax Solar", "NW Jax"]])
 col1, col2 = st.columns(2)
 col1.write(baseDF)
 col2.write(weatherDF)
 
+
 # Combine the data frames
-combinedDF = pd.merge(baseDF, weatherDF, on="DATE")
+combinedDF = pd.merge(baseDF, weatherDF, on=["DATE", "HOUR"])
+# outputs = outputs.groupby(by=["DATE", "HOUR"]).mean()
+# combinedDF["AVG_OUTPUT"] = combinedDF.groupby("DATE").mean()
 
-st.write(combinedDF)
-
-# combinedDF.to_csv("./data.csv")
+st.table(combinedDF)
+combinedDF = combinedDF.fillna(0)
+combinedDF.to_csv("./data.csv")
--- test.py
+++ test.py
@@ -3,3 +3,4 @@ import pandas as pd
 
 df = pd.read_csv("./data.csv")
 st.table(df)
+
